2b9fd5fb8693289b6e6144a6d96568f8461b9b7f
christo03 <cyrilchristo03@gmail.com>
2016-10-23 00:49:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+vapcounter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+     res.send(counter.tostring());
+ });
+
 app.get('/article-one', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
 });
