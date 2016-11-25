b660ce80b595e89b7d317105ec683dd95e9f215c
christo03 <cyrilchristo03@gmail.com>
2016-11-25 13:24:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,10 @@ app.get('/ui/style.css', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/stylebty.css', function (req, res) {
+ res.sendFile(path.join(__dirname, 'ui', 'stylebty.css'));
+});
+
 app.get('/ui/main.js', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
